946f9c9a07f1156ed403a934e46ed4de13641a29
ks603 <kevin.shaughnessy18@gmail.com>
2020-04-23 22:03:45 -0400

    Created attack and a strong attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,12 +8,34 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
+function attackMonster(mode) {
+  let maxDamage;
+ if (mode === 'ATTACK') {
+   maxDamage = ATTACK_VALUE;
+ } else if (mode === 'STRONG_ATTACK') {
+   maxDamage = STRONG_ATTACK_VALUE;
+ }
+ const damage = dealMonsterDamage(maxDamage);
+ currentMonsterHealth -= damage;
+ const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+ currentPlayerHealth -= playerDamage;
+ if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+   alert('player won!')
+ } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+   alert('you Lost!')
+ } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+   alert('Tie')
+ }
+}
+
+
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  if (currentMonsterHealth <= 0) {
-    console.log('player won!')
-  }
+  attackMonster('ATTACK')
+}
+
+function strongAttackHandler() {
+ attackMonster('STRONG_ATTACK')
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
